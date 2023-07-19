f957133b75993bc212bac22ae97357cba778a295
mhmd elngar <mohmedmatrix999@gmail.com>
2023-07-19 10:02:07 +0300

    q3 done'

--- query/q3i.js
+++ query/q3i.js
@@ -1,5 +1,36 @@
 // Task 3i
 
-db.todo.aggregate([
+db.credits.aggregate([
     // TODO: Write your query here
+    {$match: {cast: {$elemMatch:  {id:7624}}}},
+    { $project: {_id: 0,crew:0 }},
+        { $unwind: { path: "$cast"} },
+        {
+                $project: {
+                        _id: "$cast.id", // explicitly project out this field
+                        character: "$cast.character",
+                        movieId: 1,
+                }},
+            {$match: {_id:7624}},
+             {
+                     $lookup: {
+                         from: "movies_metadata", // Search inside movies_metadata
+                         localField: "movieId", // match our _id
+                         foreignField: "movieId", // with the "movieId" in movies_metadata
+                         as: "movies" // Put matching rows into the field "movies"
+                     }
+                 },
+
+{
+        $project: {
+                _id: 0, // expliAcitly project out this field
+                title: {$first: "$movies.title"}, // grab the title of first movie
+                release_date: {$first: "$movies.release_date"}, // rename count to num_ratings
+                character: 1,
+        }
+     },
+     {$sort:{release_date:-1}},
+
+
+
 ]);
--- query/q3ii.js
+++ query/q3ii.js
@@ -1,5 +1,32 @@
 // Task 3ii
 
-db.todo.aggregate([
+db.credits.aggregate([
     // TODO: Write your query here
+    {
+            $match: {
+                crew: {
+                    $elemMatch: {
+                        job: "Director",
+                        id: 5655}
+                }}
+        },
+                    { $unwind: { path: "$cast"} },
+                    { $group: {
+                                 _id: {id: "$cast.id", name: "$cast.name"},
+                                 count: {$sum: 1} // Get the count for each group
+                             }},
+ {
+                $project: {
+                count :1,
+                        id: "$_id.id", // explicitly project out this field
+                        name: "$_id.name",
+                        _id:0
+
+                }},
+     {$sort:{count:-1,id:1}},
+          {$limit: 5},
+
+
+
+
 ]);
